8d94faa398fe85e775655d99ddb01ac5dc1e2209
Lucas Ballester <lucasgasparbal@gmail.com>
2019-11-19 21:42:39 -0300

    agregadas funciones a ColeccionUnidades

--- src/main/model/AlgoChess/Unidades/ColeccionUnidades.java
+++ src/main/model/AlgoChess/Unidades/ColeccionUnidades.java
@@ -1,11 +1,24 @@
 package model.AlgoChess.Unidades;
 
+import model.AlgoChess.Excepciones.CoordenadaFueraDeRangoExcepcion;
+import model.AlgoChess.Tablero.Casillero;
+
 import java.util.ArrayList;
 
 public class ColeccionUnidades {
     private ArrayList<Unidad> unidades = new ArrayList<Unidad>();
 
-    public boolean estaVacia(){
+    public boolean estaVacia() {
         return unidades.isEmpty();
+    }
+
+    public void agregarUnidad(Unidad unidad){
+        unidades.add(unidad);
+    }
+    public boolean hayUnidadesAdyacentesA(Casillero unCasillero) throws CoordenadaFueraDeRangoExcepcion {
+        for(int i = 0; i < unidades.size(); i++){
+            if(unidades.get(i).esAdyacenteA(unCasillero)){ return true;}
+        }
+        return false;
     }
 }
--- src/test/AlgoChessTest/UnidadesTest/ColeccionUnidadesTest.java
+++ src/test/AlgoChessTest/UnidadesTest/ColeccionUnidadesTest.java
@@ -1,15 +1,39 @@
 package AlgoChessTest.UnidadesTest;
 
+import model.AlgoChess.Excepciones.CoordenadaFueraDeRangoExcepcion;
+import model.AlgoChess.Tablero.Casillero;
 import model.AlgoChess.Unidades.*;
 import org.junit.jupiter.api.*;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+
 public class ColeccionUnidadesTest {
+
+    Casillero casillero = mock(Casillero.class);
+    Soldado soldado = mock(Soldado.class);
+
     @Test
     public void ColeccionUnidadesSeIniciaVacia(){
 
         ColeccionUnidades coleccion = new ColeccionUnidades();
 
         Assertions.assertTrue(coleccion.estaVacia());
+    }
+
+    @Test
+    public void ColeccionUnidadesConUnidadAgregadaNoEstaVacia(){
+
+        ColeccionUnidades coleccion = new ColeccionUnidades();
+
+        coleccion.agregarUnidad(soldado);
+        Assertions.assertFalse(coleccion.estaVacia());
+    }
+
+    @Test
+    public void ColeccionUnidadesHayUnidadesAdyacentesDevuelveFalseEnUnaColeccionSinUnidades() throws CoordenadaFueraDeRangoExcepcion {
+        ColeccionUnidades coleccion = new ColeccionUnidades();
+
+        Assertions.assertFalse(coleccion.hayUnidadesAdyacentesA(casillero));
     }
 }
